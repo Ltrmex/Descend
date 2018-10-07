40a334eec60b62207efd11e3aca0d74f35fa44f0
Ltrmex <maciekmajchrzak23@wp.pl>
2018-10-07 20:21:17 +0100

    camera movement added

---
+++ Descend/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    private float speed = .5f;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("MoveCamera", 1, 0.5f);
+    }
+
+    void MoveCamera() {
+        transform.position = new Vector3(0, transform.position.y - speed, -9);
+    }
+}
