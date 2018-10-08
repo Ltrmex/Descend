3c9edac9cd628230b789bd26c3f1ee5e66b629f5
Ltrmex <maciekmajchrzak23@wp.pl>
2018-10-08 14:22:39 +0100

    basic game over

---
+++ Descend/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private bool collision = false;
+    public GameObject gameOver;
+
+    // Update is called once per frame
+    void Update() {
+        //check for player's collision with game object tagged Dock
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Dead Zone")
+        {
+            collision = true;
+            gameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
