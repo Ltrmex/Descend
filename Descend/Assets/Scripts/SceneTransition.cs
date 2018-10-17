2a63c0d140ee7a8611a2d145248e796d6d6cfee2
Ltrmex <maciekmajchrzak23@wp.pl>
2018-10-17 15:02:00 +0100

    switching between level scenes

---
+++ Descend/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void PickLevel(int levelNum) {
+        string sceneName;
+
+        switch (levelNum) {
+            case 1:
+                sceneName = "Green Zone";
+                break;
+            case 2:
+                sceneName = "Yellow Zone";
+                break;
+            case 3:
+                sceneName = "Blue Zone";
+                break;
+            case 4:
+                sceneName = "Red Zone";
+                break;
+            default:
+                sceneName = "Green Zone";
+                break;
+        }   //  switch
+
+        SwitchLevel(sceneName);
+    }   //  PickLevel()
+
+    private void SwitchLevel(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }   //  SwitchLevel()
+}
