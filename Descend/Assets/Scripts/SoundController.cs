2634fe441d0b6134a05151a69b22c8980b08478f
Ltrmex <maciekmajchrzak23@wp.pl>
2018-11-14 18:49:36 +0000

    audio added

---
+++ Descend/Assets/Scripts/SoundController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+//  CODE ADAPTED FROM: https://www.youtube.com/watch?v=82Mn8v55nr0
+
+public class SoundController : MonoBehaviour
+{
+    private static SoundController instance = null;
+    private int count;
+    public AudioSource backgroundMusicSource;
+
+    public static SoundController Instance
+    {
+        get { return instance; }
+    }
+
+    void Start()
+    {
+        count = 0;
+        backgroundMusicSource.volume = 0.5f;
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }   //  Awake()
+
+    public void Mute()
+    {
+        if (count == 0)
+        {
+            count = 1;
+            backgroundMusicSource.volume = 0;
+        }
+        else
+        {
+            count = 0;
+            backgroundMusicSource.volume = 0.5f;
+        }
+    }
+} //  SoundController
---
+++ Descend/Assets/Scripts/SoundEffects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour {
+    public AudioClip clickSound;
+    public AudioSource clickSource;
+
+    private static SoundEffects instance = null;
+
+    // Use this for initialization
+    void Start () {
+        clickSource = GetComponent<AudioSource>();
+    }
+
+    public void ButtonClick() {
+        clickSource.PlayOneShot(clickSound);
+    }
+
+}
